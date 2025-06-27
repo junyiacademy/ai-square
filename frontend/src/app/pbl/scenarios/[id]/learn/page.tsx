4c0e94366dafcb7838e660f9a382c3c39011e2ae
Youngger9765 <purpleice9765@msn.com>
2025-06-27 12:12:45 +0800

    fix: Add missing closing span tag in learn page JSX

--- frontend/src/app/pbl/scenarios/[id]/learn/page.tsx
+++ frontend/src/app/pbl/scenarios/[id]/learn/page.tsx
@@ -1441,7 +1441,7 @@ export default function PBLLearnPage() {
                   <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                   </svg>
-                  <span>{t('learn.taskAnalyzed')}
+                  <span>{t('learn.taskAnalyzed')}</span>
                   {/* Show re-analyze hint if conversation continued */}
                   {conversation.filter(msg => msg.role === 'user').length > 
                     (stageAnalysis.performanceMetrics?.interactionCount || 0) && (
